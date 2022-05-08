897ca8423318c60291cf921aa474fbfed85a4ae4
Huggo <77754479+Zapatron@users.noreply.github.com>
2022-05-08 19:06:38 +0200

    Create Kick.lua

---
+++ Plutonium Loader/Utillities/Kick.lua
@@ -0,0 +1,12 @@
+pcall(function()
+game.Players.LocalPlayer:Kick([[
+ㅤ
+ㅤ
+Join the Discord for a new key!
+(It's also been copied if your exploit supports it!)
+https://discord.gg/qqPFjvxkJx
+
+(If its not your key its because this game is unsupported!)
+]])
+setclipboard("https://discord.gg/qqPFjvxkJx")
+end)

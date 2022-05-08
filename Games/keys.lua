6799a2b1c115f07d777529f4c10977e768bdc1b0
Huggo <77754479+Zapatron@users.noreply.github.com>
2022-05-08 18:56:54 +0200

    Delete Games directory

--- Games/keys.lua
+++
@@ -1,6 +0,0 @@
-Keys_Table = {
-    ["plutonium_xg1w5t"] = true
-    ["TestKey"] = true --Urban's perma key, you can ofc use this but I'll probably change it if I see its use from time to time.
-}
-
-return Keys_Table
